92e622f012db990963c16c8de826ae4018feb2a8
Nimeshamadhushan <89959527+Nimeshamadhushan@users.noreply.github.com>
2025-02-02 02:36:05 +0530

    Tharu md 😇❤️

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'THARU',
+  author: 'MD',
+  botName: "THARU-MD",
+  botOwner: 'THARU-MD', // Your name
+  ownerNumber: '94789549564', //Your number
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
