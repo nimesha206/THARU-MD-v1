5ce99af231a2b79eb6591b9b7eee609d779cad8e
Nimeshamadhushan <89959527+Nimeshamadhushan@users.noreply.github.com>
2025-02-01 22:13:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 👩‍💻",
+SESSION_ID: process.env.SESSION_ID || "MALAKA-MD~SVMRyajC#w_32kVjzrJPGsTjNwcfaRhAkguB5yeHazbFvcKBFlNw",
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS === undefined ?"true" : process.env.AUTO_REACT_STATUS,
